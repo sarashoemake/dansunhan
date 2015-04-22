739cc1e4ed31c31ac3f05bd33cf35938a95f6eb5
sarashoemake <sara.shoemake@gmail.com>
2015-04-22 14:21:12 -0400

    Create castling_type.cpp

---
+++ castling_type.cpp
@@ -0,0 +1,15 @@
+/*
+ * castling_type.cpp
+ *
+ *  Created on: Apr 21, 2015
+ *      Author: sarashoemake
+ */
+
+#include "castling_type.h"
+
+using namespace std;
+
+const array<int, CastlingType::VALUES_SIZE> CastlingType::values = {
+		KINGSIDE, QUEENSIDE
+};
+
